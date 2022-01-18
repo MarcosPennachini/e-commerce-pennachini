8d00a232d1c9b133a7898915a118d2b2da39e95f
MarcosPenna <marcoskpo_penpun@hotmail.com>
2022-01-17 23:56:47 -0300

    Formatting navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container, Flex } from '@chakra-ui/react';
+import { Container, Flex, Heading } from '@chakra-ui/react';
 
 const NavBar = () => {
     return (
-        <Container maxW='container.xl'>
+        <Container maxW='container.lg'>
             <Flex w='full' justifyContent='space-between'>
-
+                <Heading size='xl'>
+                    NavBar
+                </Heading>
             </Flex>
         </Container>
     );
