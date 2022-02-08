51acb800850ca011bdde96610b0535f77b00c462
MarcosPenna <marcoskpo_penpun@hotmail.com>
2022-02-07 22:30:48 -0300

    Created ItemDetailContainer component. Also created script getItem. It returns a promise with 1 product

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,35 +1,52 @@
-import { Badge, Box, HStack } from '@chakra-ui/react';
+import { Badge, Box, HStack, Image, Text, useToast } from '@chakra-ui/react';
 import React from 'react';
+import ItemCount from './ItemCount';
 
-const ItemDetail = ({ pictureUrl, description, price, title, stock }) => {
+const ItemDetail = ({ item }) => {
+  const product = item;
+  const toast = useToast();
+
+  const onAdd = (cant) => {
+    console.log('Cantidad seleccionada: ' + cant);
+    toast({
+      title: 'Producto agregago',
+      description: 'Se ha agregado al carrito!',
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    });
+  };
+
   return (
     <HStack w='full' px={4} py={3} overflow='hidden'>
-      <Box>
-        <Image src={pictureUrl} alt='Imágen de producto' />
+      <Box w='full'>
+        <Image src={product.pictureUrl} alt='Imágen de producto' backgroundColor='gray.100' />
       </Box>
 
-      <Box p={4}>
+      <Box p={4} w='full'>
         <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius='full' px='2' colorScheme='purple'>
+          <Badge borderRadius='full' px='2' mr={2} colorScheme='purple'>
             Nuevo
           </Badge>
           <Badge borderRadius='full' px='2' colorScheme='blue'>
-            Stock: {stock}
+            Stock: {product.stock}
           </Badge>
         </Box>
 
-        <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' isTruncated>
-          {title}
+        <Box mt='1' fontWeight='semibold' fontSize='4xl' as='h4'>
+          {product.title}
         </Box>
 
-        <Text fontSize='2xl' as='h5' lineHeight='tight'>
-          {`$${parseFloat(price)}`}
+        <Text fontSize='2xl' as='h5' mb={3}>
+          {`$${parseFloat(product.price)}`}
         </Text>
 
-        <Text as='p'>{description}</Text>
+        <Text as='p'>{product.description}</Text>
+
+        <ItemCount initial={1} stock={product.stock} onAdd={onAdd} />
       </Box>
     </HStack>
-  );
+  )
 };
 
 export default ItemDetail;
