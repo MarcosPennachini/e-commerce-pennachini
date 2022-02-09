b9fe3370c00d78db0bb41810c7bd8506c3030f72
MarcosPenna <marcoskpo_penpun@hotmail.com>
2022-02-09 12:45:39 -0300

    Fixed ItemDetailContainer. Finished route to ItemDetail when clicking Ver mas in each item listed on home page

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,58 +1,38 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ItemDetail from './ItemDetail';
-import { Container, Text } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import getProductById from '../helpers/getProductById';
-
+import { products } from '../products';
 
 const ItemDetailContainer = () => {
-  //const [item, setItem] = useState();
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
-  console.log(id);
 
-  const product = useMemo(() => getProductById(id), [id]);
-  console.log(product)
+  const getItem = (id) => {
+    products
+      .then((result) => {
+        console.log(result);
+        const data = result.filter((item) => item.id === id);
+        console.log(data);
+        setItem(data);
+      })
+      .catch((error) => {
+        console.error('Se produjo un error al obtener el Item ', error);
+      })
+      .then(() => setLoading(false));
+  };
 
-  // const getItem = (id) => {
-  //   products
-  //     .then((result) => {
-  //       console.log(result);
-  //       const data = result.filter((item) => item.id === id);
-  //       console.log(data);
-  //       setItem(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Se produjo un error al obtener el Item ', error);
-  //     });
-  // }
-  
-
-  // useEffect(() => {
-
-  //   if (id) {
-  //     getItem(id);
-  //   }
-
-  // }, [id, item]);
-
-
-  // console.log(item)
-  
+  useEffect(() => {
+    if (id) {
+      getItem(id);
+    }
+  }, [id]);
 
   return (
     <section>
-      <Text
-        mb={4}
-        bgGradient='linear(to-l, #0093E9, #80D0C7)'
-        bgClip='text'
-        textAlign='center'
-        fontSize='4xl'
-        fontWeight='900'
-      >
-        Detalle de producto
-      </Text>
       <Container maxW='container.lg' padding={[1, 4]}>
-        <ItemDetail item={product} />
+        {!loading && <ItemDetail item={item} />}
       </Container>
     </section>
   );
