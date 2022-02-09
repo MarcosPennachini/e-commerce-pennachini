bef33663884d8e9c7813532e8b6f12e936ae36fa
MarcosPenna <marcoskpo_penpun@hotmail.com>
2022-02-09 19:37:14 -0300

    Created hero component. Minor ui fixes

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,7 +2,7 @@ import { Badge, Box, Image, Stack, Text, useToast, useColorModeValue } from '@ch
 import ItemCount from './ItemCount';
 
 const ItemDetail = ({ item }) => {
-  const backgroundColor = useColorModeValue('radial-gradient( circle farthest-corner at 18.7% 37.8%,  rgba(250,250,250,1) 0%, rgba(225,234,238,1) 90% )', 'radial-gradient( circle farthest-corner at 32.7% 82.7%,  rgba(173,0,171,1) 8.3%, rgba(15,51,92,1) 79.4% )');
+  const backgroundColor = useColorModeValue('radial-gradient( circle 1224px at 10.6% 8.8%,  rgba(255,255,255,1) 0%, rgba(153,202,251,1) 100.2% );', 'radial-gradient( circle farthest-corner at 32.7% 82.7%,  rgba(173,0,171,1) 8.3%, rgba(15,51,92,1) 79.4% )');
   const [product] = item;
   const toast = useToast();
 
