cd35692dd4af2c21dd9c046a8c398bfed8a82948
MarcosPennachini <marcospennachini@gmail.com>
2022-02-16 23:53:52 -0300

    Using context in ItemDetail component, on onAdd event. Rendering Ver carrito button when adding items

---
+++ src/components/CartDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartDetail = () => {
+  return (
+    <h2>CartDetail</h2>
+  )
+}
+
+export default CartDetail
--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from 'react';
 
 export const CartContext = createContext();
 
@@ -8,58 +8,66 @@ export const CartContext = createContext();
 //     totalPrice: 0
 // }
 
-export const CartProvider = ({children}) => {
-    const [items, setItems] = useState([]);
-    const [totalQuantity, setTotalQantity] = useState(0);
-    const [totalPrice, setTotalPrice] = useState(0);
+export const CartProvider = ({ children }) => {
+  const [items, setItems] = useState([]);
+  const [totalQuantity, setTotalQantity] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-    const addItem = (item, cant) => {
-        const index = isInCart(item);
-        if (index > -1) {
-            //Si el item ya está en el carrito
-            let updatedITems = [...items];
-            updatedITems[index].quantity += cant;
-            if (updatedITems[index].quantity > updatedITems[index].stock) {
-                throw new Error(`La cantidad elegida no puede superar el stock (${updatedITems[index].stock})`);
-            } else {
-                setItems(updatedITems);
-            }
-        } else {
-            //Si el item no está en el carrito
-            const newItem = {...item, quantity: cant};
-            setItems(...items, newItem);
-        }
+  const addItem = (item, cant) => {
+    const index = isInCart(item);
+    if (index > -1) {
+      //Si el item ya está en el carrito
+      let updatedITems = [...items];
+      updatedITems[index].quantity += cant;
+      if (updatedITems[index].quantity > updatedITems[index].stock) {
+        throw new Error(
+          `La cantidad elegida no puede superar el stock (${updatedITems[index].stock})`
+        );
+      } else {
+        setItems(updatedITems);
+      }
+    } else {
+      //Si el item no está en el carrito
+      const newItem = {...item};
+      newItem.quantity = cant;
+    //   console.log(newItem);
+      setItems(...items, newItem);
     }
+  };
 
-    const removeItem = (itemToRemove) => {
-        const filteredItems = items.filter((item) => item.id != itemToRemove.id);
-        setItems(filteredItems);
-    }
+  const removeItem = (itemToRemove) => {
+    const filteredItems = items.filter((item) => item.id !== itemToRemove.id);
+    setItems(filteredItems);
+  };
 
-    const isInCart = (newItem) => {
-        const itemFinded = items.find((i) => i.id === newItem.id);
-        return items.indexOf(itemFinded);
-    }
+  const isInCart = (newItem) => {
+    const itemFinded = items.filter((i) => i.id === newItem.id);
+    return items.indexOf(itemFinded);
+  };
 
-    const clearCart = () => {
-        setItems([]);
-    }
+  const clearCart = () => {
+    setItems([]);
+  };
 
-    /** Get total cart quantity */
-    const getTotalPrice = () => {
-        totalPrice.map((item) => setTotalPrice(totalPrice + (item.price * item.quantity)));
-        return totalPrice;
-    }
+  /** Get total cart quantity */
+  const getTotalPrice = () => {
+    totalPrice.map((item) => setTotalPrice(totalPrice + item.price * item.quantity));
+    return totalPrice;
+  };
 
-    /** Get total cart price */
-    const getTotalQuantity = () => {
-        totalQuantity.map((item) => setTotalQantity(totalQuantity + item.quantity));
-        return totalQuantity;
-    }
+  /** Get total cart price */
+  const getTotalQuantity = () => {
+    totalQuantity.map((item) => setTotalQantity(totalQuantity + item.quantity));
+    return totalQuantity;
+  };
 
-    return(
-        <CartContext.Provider value={}>
-            {children}
-        </CartContext.Provider>
-    );
-}
+  console.log(items)
+
+  return (
+    <CartContext.Provider
+      value={{ items, addItem, removeItem, clearCart, getTotalPrice, getTotalQuantity }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
